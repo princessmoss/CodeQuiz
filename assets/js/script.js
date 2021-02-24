b6dc4cabd7b908c8ba41420f41100b114fd77e08
Princess M <princessmosse@gmail.com>
2021-02-24 18:24:00 -0500

    adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,11 @@ var questionIndex = 0;
 // Define a set of questions
 const questions = [
     {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a. onclick"
+    },
+    {
         question: "Inside which HTML element do we put the JavaScript?",
         choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
         answer: "d. <script>"
@@ -48,24 +53,9 @@ const questions = [
         answer: "c. quotes"
     },
     {
-        question: "Arrays in JavaScript can be used to store _____.",
-        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
-        answer: "b. other arrays"
-    },
-    {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
-        answer: "c. alerts"
-    },
-    {
         question: "How do you create a function in JavaScript",
         choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
         answer: "b. function myFunction()"
-    },
-    {
-        question: "How do you call a function named myFunction?",
-        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
-        answer: "c. myFunctions()"
     },
     {
         question: "To see if two variables are equal in an if / else statement you would use ____.",
@@ -73,28 +63,18 @@ const questions = [
         answer: "b. =="
     },
     {
-        question: "The first index of an array is ____.",
-        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
-        answer: "a. 0"
+        question: "What is used primarily to add styling to a web page?",
+        choices: ["a. HTML", "b. Images", "c. CSS", "d. README"],
+        answer: "c. CSS"
     },
     {
-        question: "Who invented JavaScript?",
-        choices: ["a. Douglas Crockford", "b. Sheryl Sandberg", "c. Brendan Eich", "d. Ben Javascript"],
-        answer: "c. Brendan Eich"
+        question: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
     },
     {
-        question: "How to write an IF statement in JavaScript?",
-        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
-        answer: "c. if(i == 5)"
-    },
-    {
-        question: "How do you add a comment in a JavaScript?",
+        question: "How do you add a comment in a HTML file?",
         choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
-        answer: "a. //This is a comment"
+        answer: "b. <!--This is a comment-->"
     },
-    {
-        question: "Which event occurs when the user clicks on an HTML element?",
-        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-        answer: "a. onclick"
-    }
 ];
