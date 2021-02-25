24ab58ef33ac4e8a879381f29c6d0d11e92192bb
Princess M <princessmosse@gmail.com>
2021-02-24 22:28:14 -0500

    changing style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,362 @@
-// grab references to elements
-var timer = document.getElementById("timer");
-var timeLeft = document.getElementById("timeLeft");
-var timesUp = document.getElementById("timesUp");
+// Timer setup
+var startButton = document.getElementById("start-button");
 
-var startDiv = document.getElementById("start");
-var startQuizBtn = document.getElementById("start-quiz-button");
+var timeLeft = document.getElementById("timer-count");
 
-var questionDiv = document.getElementById("questionDiv");
-var questionTitle = document.getElementById("questionTitle");
-var choiceA = document.getElementById("btn0");
-var choiceB = document.getElementById("btn1");
-var choiceC = document.getElementById("btn2");
-var choiceD = document.getElementById("btn3");
-var answerCheck = document.getElementById("answerCheck");
+var content = document.getElementById("content");
 
-var summary = document.getElementById("summary");
-var submitInitialBtn = document.getElementById("submitInitialBtn");
-var initialInput = document.getElementById("initialInput");
-var everything = document.getElementById("everything");
+var scorePage = document.getElementById("score-page");
 
-var highScoreSection = document.getElementById("highScoreSection");
-var finalScore = document.getElementById("finalScore");
+// Starting time for quiz
+var secondsLeft = 76;
 
-var goBackBtn = document.getElementById("goBackBtn");
-var clearHighScoreBtn = document.getElementById("clearHighScoreBtn"); 
-var viewHighScore = document.getElementById("viewHighScore");
-var listOfHighScores = document.getElementById("listOfHighScores");
+var questionCounter = 0;
 
-// define other variables
-var correctAns = 0;
-var questionNum = 0;
-var scoreResult;
-var questionIndex = 0;
+var timerInterval;
+
+// Timer functionality
+function setTime() {
+    
+    timerInterval = setInterval(function() {        
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft;
+
+        // Stop timer when reach zero
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            secondsLeft = 0;
+        } 
+    }, 1000);
+    
+}
+
+var answers;
+
+// function to generate HTML for the questions
+function generateQuestions(questionObject) {
+
+    // console.log(questionObject);
+
+    var question = document.createElement("h3");
+    question.setAttribute("class", "question");
+    question.textContent = questionObject.question;
+    content.appendChild(question);
+
+    answers = Object.entries(questionObject.answers);
+
+    // console.log(answers);
+
+    for (var i=0; i < answers.length; i++) {
+        var answer = answers[i];
+        // answer = ["1", "answer text"]
+        var button = document.createElement("button");
+        button.textContent = answer[0] + ". " + answer[1];
+        button.setAttribute("value", answer[0]);
+        button.setAttribute("id", "answer-btn");
+        button.setAttribute("class", "btn btn-md btn-secondary")
+        button.addEventListener("click", function (event) {
+            answerSelect(event, questionObject.correctAnswer)
+        });
+        content.appendChild(button);
+    };
+
+}
+
+var rightAnswer;
+
+// what happens when user selects an answer
+function answerSelect(event, correctAnswer) {
+
+    var chosenAnswer = event.target.value;
+
+    if (chosenAnswer === correctAnswer) {
+        rightAnswer = true;
+        questionCounter++;
+        content.innerHTML = "";
+        if (questionCounter <= answers.length) {
+            generateQuestions(quizQuestions[questionCounter]);
+            rightWrongText();
+        } else {
+            endGame();
+        }
+    } else {
+        if (secondsLeft >= 10){
+            secondsLeft -= 10;
+        } else if (secondsLeft < 10 || secondsLeft > 0) {
+            secondsLeft = 0;
+        } else {
+            secondsLeft = 0;
+        }
+        
+        questionCounter++;
+        
+        rightAnswer = false;
+        content.innerHTML = "";
+        if (questionCounter <= answers.length) {
+            generateQuestions(quizQuestions[questionCounter]);
+            rightWrongText();
+        } else {
+            endGame();
+        }
+        
+    }
+
+}
+
+function rightWrongText() {
+    var correct = document.createElement("p");
+
+    if (rightAnswer) {
+        correct.setAttribute("class", "right-wrong-text");
+        correct.textContent = "Correct answer!";
+        content.appendChild(correct);
+    } else {
+        correct.setAttribute("class", "right-wrong-text");
+        correct.textContent = "Wrong answer!";
+        content.appendChild(correct);
+    } 
+    
+    setTimeout( function () {
+        correct.setAttribute("class", "right-wrong-text hide");
+    }, 1000);
+    
+}
+
+
+var listOfScoreObjects = [];
+
+var listOfScores = new Object();
+
+var score;
+
+function endGame() {
+    clearInterval(timerInterval);
+    score = secondsLeft;
+    timeLeft.textContent = "";
+    content.innerHTML = "";
+
+    var timerEl = document.querySelector(".timer");
+    timerEl.textContent = "";
+
+    var gameOverText = document.createElement("h2");
+    gameOverText.textContent = "GAME OVER";
+    content.appendChild(gameOverText);
+
+    var yourScoreText = document.createElement("p");
+    yourScoreText.textContent = "Final Score: " + score;    
+    content.appendChild(yourScoreText);
+
+    initialEnter();
+
+}
 
 
 
-// Define a set of questions
-const questions = [
+var lastScore;
+
+var nameInput;
+
+var rawInitials;
+
+function initialEnter() {
+
+    // create form to hold initials stuff
+    var initialsForm = document.createElement("form");
+    initialsForm.setAttribute("class", "initials-form");
+    // content.appendChild(initialsForm);
+
+
+    // create text before initials box
+    var enterText = document.createElement("label");
+    enterText.textContent = "Enter Initials: "
+    initialsForm.appendChild(enterText);
+
+    // create input for initials
+    nameInput = document.createElement("input");
+    nameInput.setAttribute("id", "name-input");
+    nameInput.textContent = "Enter Initials: ";
+    initialsForm.appendChild(nameInput);
+
+    // create button to submit 
+    var nameSubmitBtn = document.createElement("button");
+    nameSubmitBtn.textContent = "Submit";
+    nameSubmitBtn.setAttribute("id", "initials-submit");
+    nameSubmitBtn.setAttribute("class", "btn btn-sm btn-primary")
+    initialsForm.appendChild(nameSubmitBtn);
+    content.appendChild(initialsForm);
+
+    
+
+    var initialsInputEl = document.querySelector(".initials-form");
+
+    initialsInputEl.addEventListener("submit", handleInitialSubmit);
+
+}
+
+var displayScores;
+
+if (scorePage) {
+
+    displayScores = JSON.parse(localStorage.getItem("unordered-scores"));
+    // storeData();
+    
+    var backBtnEl = document.getElementById("back-btn");
+    backBtnEl.addEventListener("click", returnToQuiz);
+
+    var clearBtnEl = document.getElementById("clear-btn");
+    clearBtnEl.addEventListener("click", clearScores)
+
+}
+
+function returnToQuiz(event) {
+    event.preventDefault();
+
+    document.location.href = ("index.html");
+}
+
+function clearScores(event) {
+    event.preventDefault();
+    localStorage.clear();
+    scorePage.textContent="";
+}
+
+// function storeData () {
+
+//     console.log(unorderedScores);
+
+//     localStorage.setItem("scores", JSON.stringify(listOfScores));
+// }
+
+var unorderedScores = [];
+
+function handleInitialSubmit(event) {
+
+    event.preventDefault();
+    
+    rawInitials = document.getElementById("name-input").value;
+    
+    listOfScores[score] = rawInitials;
+
+    unorderedScores.push({"score": score, "name": rawInitials,});
+
+    localStorage.setItem("unordered-scores", JSON.stringify(unorderedScores));
+    
+
+    // open next html page with high scores list
+    document.location.href = ("scores.html");
+
+
+}
+
+function init() {
+    var storedScores = JSON.parse(localStorage.getItem("unordered-scores"));
+
+    if (storedScores !==null) {
+        unorderedScores = storedScores;
+    }
+
+    if (scorePage) {
+        // console.log(unorderedScores.sort(compare).reverse());
+
+        var sortedScores = unorderedScores.sort(compare).reverse();
+
+        for (var i=0; i < sortedScores.length; i++) {
+            var scoreObject = sortedScores[i];
+            // console.log(scoreObject)
+
+            var scoreFromObject = scoreObject["score"];
+            var nameFromObject = scoreObject["name"];
+
+            var li = document.createElement("li");
+            li.textContent = scoreFromObject + " - " + nameFromObject;
+            li.setAttribute("data-index", i);
+            scorePage.appendChild(li);
+
+        }
+    }
+}
+
+var sortedScores;
+
+
+// source: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+function compare(a,b) {
+    var comparison = 0;
+    var scoreA = a.score;
+    var scoreB = b.score;
+
+    if (scoreA > scoreB) {
+        comparison = 1;
+    } else if (scoreA < scoreB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function startQuiz() {
+    setTime();
+    content.innerHTML = "";
+    generateQuestions(quizQuestions[questionCounter]);
+}
+
+// When start clicked, start the quiz game (start timer and load questions)
+if (startButton) {
+    startButton.addEventListener("click", startQuiz);
+}
+
+init();
+
+// Establish questions and answers as objects in an array
+// the answers are another object
+var quizQuestions = [
     {
-        question: "Which event occurs when the user clicks on an HTML element?",
-        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-        answer: "a. onclick"
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers",
+        },
+        correctAnswer: "3"
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
-        answer: "d. <script>"
+        question: "The condition in an if/else statement is contained in:",
+        answers: {
+            1: "quotes",
+            2: "curly braces",
+            3: "parenthesis",
+            4: "square brackets",
+        },
+        correctAnswer: "3"
     },
     {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
-        answer: "c. quotes"
+        question: "Arrays in JavaScript can be used to store:",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+        },
+        correctAnswer: "4"
     },
     {
-        question: "How do you create a function in JavaScript",
-        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-        answer: "b. function myFunction()"
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly braces",
+            3: "quotes",
+            4: "parenthesis",
+        },
+        correctAnswer: "3"
     },
     {
-        question: "To see if two variables are equal in an if / else statement you would use ____.",
-        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
-        answer: "b. =="
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log",
+        },
+        correctAnswer: "4"
     },
-    {
-        question: "What is used primarily to add styling to a web page?",
-        choices: ["a. HTML", "b. Images", "c. CSS", "d. README"],
-        answer: "c. CSS"
-    },
-    {
-        question: "Arrays in Javascript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
-    {
-        question: "How do you add a comment in a HTML file?",
-        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
-        answer: "b. <!--This is a comment-->"
-    },
-];
+]
